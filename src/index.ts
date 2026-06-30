dca4310e915dc50e7a3a2b53435299403a7a1c23
cata36 <cataroitman@gmail.com>
2026-06-30 15:30:39 -0300

    final version

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,58 @@
 import { preguntar } from './lib/consola.ts';
 
+
 function repetirLetra(letra: string, cantidad: number): string {
-  // COMPLETAR
+
+
+  let i = 0;
+  let res = "";
+
+
+//   while (i <= cantidad)
+// {
+//   let u = 0;
+//   while (u < cantidad)
+//   {
+//     res+= letra;
+//     u++;
+//   }
+//   if (u === cantidad )
+// {
+//   u = 0;
+//   res += "\n";
+// }
+// i++;
+// }
+
+while (i <= cantidad)
+{
+  i++;
+  let u = 0;
+
+  while (u < cantidad)
+  {
+    res += letra;
+    u++;
+
+  }
+  if(u === cantidad)
+  {
+    u = 0;
+    res += "\n";
+  }
+
+
+
 }
+
+
+
+return res;
+}
+
 
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el cuadrado? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
 
-// COMPLETAR
+
+console.log(repetirLetra(caracter, tamaño));
